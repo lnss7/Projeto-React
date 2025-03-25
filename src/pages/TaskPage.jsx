8d7ac5edda4f64398b01b30b4fb7d83c4001cbf2
Lucas Simão <novellysimaolucas@gmail.com>
2025-03-25 11:04:28 -0300

    correcao de bugs

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -13,7 +13,7 @@ function TaskPage() {
   }
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <button 
           onClick={onBackClick}
